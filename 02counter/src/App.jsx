d480507ea92ae7c4ed8b014993001940d15376a5
sadat8418 <sadat.arefin98@gmail.com>
2024-11-30 00:00:20 +0600

    counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,13 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  let [counter, sCounter]  = useState(15)  //setCounter is function who updates, 
+
+  //let counter = 15
+
+  const addValue = () => {
+    //console.log("Add value clicked",Math.random())
+    //counter = counter + 1
+    //console.log(counter) // couter console.log a bartese, UI update hocce na ... //REact controlled UI 
+    sCounter(prevCounter => prevCounter + 1)
+   //sCounter(counter +1)       //eita dile "const" deya jabe na... 
+   if (counter >= 20) {sCounter(counter=20)}
+  }
+
+  const removeValue = () => {
+    sCounter(counter - 1)
+    if (counter <= 0) {sCounter(counter=0)}
+  }
+  
   return (
     <>
-      <h1>Sadat aur react</h1>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
-}
+}//onek jaygay counter variable ,use hooks to update in UI
 
 export default App
