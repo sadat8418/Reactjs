f2d3ff63cec9d3fb43067a5277a823745ab6fb06
sadat8418 <sadat.arefin98@gmail.com>
2025-07-22 22:43:03 +0600

    before postCard

--- 12MegaBlog/src/components/post-form/PostForm.jsx
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -4,10 +4,11 @@ import { Button, Input, RTE, Select } from "..";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+//continuosly watch 
 
 export default function PostForm({ post }) {
     const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
-        defaultValues: {
+        defaultValues: { //jodi edit korte ase, default value dite hoy
             title: post?.title || "",
             slug: post?.$id || "",
             content: post?.content || "",
